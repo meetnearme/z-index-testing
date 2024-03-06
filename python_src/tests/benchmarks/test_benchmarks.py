e3cba57faffbc47b12fc89e9bce4c887683691c0
Brandon Tripp <brandongtripp@gmail.com>
2024-03-06 12:02:37 -0700

    Add the table_name arg to benchmarkbase tests

--- python_src/tests/benchmarks/test_benchmarks.py
+++ python_src/tests/benchmarks/test_benchmarks.py
@@ -7,7 +7,7 @@ from python_src.src.benchmarks.benchmarks import BenchmarkBase
 class TestBenchmarking(BenchmarkBase):
     def test_setup_method(self):
         # Test that initializes the dynamodb resource and table
-        self.setup_method()
+        self.setup_method('EventsTable')
         # Assert that the dynamodb resource and table are initialized
         assert self.dynamodb is not None
         assert self.table is not None
@@ -15,7 +15,7 @@ class TestBenchmarking(BenchmarkBase):
 
     def test_teardown_method(self):
         # test that teardown resets the dynamodb resource and table
-        self.setup_method()
+        self.setup_method('EventsTable')
         self.teardown_method()
         # Assert that the dynamodb resource and table are reset
         assert self.dynamodb is None
@@ -23,14 +23,14 @@ class TestBenchmarking(BenchmarkBase):
 
     def test_query_point_placeholder(self):
         # Test that query_point method is callable 
-        self.setup_method()
+        self.setup_method('EventsTable')
         lon, lat = -73.98517, 40.74921
         result = self.query_point(lon, lat)
         assert result is None
 
     def test_query_range_placeholder(self):
         # Test that query_range method is callable 
-        self.setup_method()
+        self.setup_method('EventsTable')
         min_lat, max_lat = 40.5, 41.2 
         min_lon, max_lon = -74.5, -73.5
         result = self.query_range(min_lat, max_lat, min_lon, max_lon)
