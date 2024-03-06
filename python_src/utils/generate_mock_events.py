ed9f323d108e6b75a281a16b911716876c491f29
Brandon Tripp <brandongtripp@gmail.com>
2024-03-06 13:04:59 -0700

    Move generate events into a utils module and rename to generate mock events

--- python_src/utils/generate_mock_events.py
+++ python_src/utils/generate_mock_events.py
@@ -1,9 +1,10 @@
 import uuid as uuid_mod
 import random
 import json
-import struct
 from datetime import datetime, timedelta
 from faker import Faker
+
+from src.indexing.z_order import calculate_z_order_index
 
 fake = Faker()
 
@@ -31,46 +32,6 @@ CITIES = [
 
 start_date = datetime(2024, 6, 1)
 end_date = start_date + timedelta(days=90)
-
-def map_float_to_sortable_int(float_value):
-    """
-    Maps a floating-point value to an unsigned integer that can be lexicographically sorted
-    """
-    float_bytes = struct.pack('>f', float_value)
-
-    # apply mapping function
-    if float_value >= 0:
-        # XOR operation for flipping first bit to make unsigned integer representation of positive float come after negative
-        sortable_bytes = bytes([(float_bytes[0] ^ 0x80)] + list(float_bytes[1:]))
-    else:
-        # XOR to flip all bits, to make negative floats come first from lowest to highest in unsigned integer representation
-        sortable_bytes = bytes([b ^ 0xFF for b in float_bytes])
-
-    # convert mapped bytes to an unsigned integer
-    sortable_int = int.from_bytes(sortable_bytes, byteorder='big', signed=False)
-
-    return sortable_int
-
-
-def calculate_z_order_index(start_time, lon, lat):
-    # Convert dimensions to binary representations
-    start_time_bin = bin(int(start_time.timestamp()))[2:].zfill(32)
-
-    # Map floating point values to sortable unsigned integers
-    lon_sortable_int = map_float_to_sortable_int(lon)
-    lat_sortable_int = map_float_to_sortable_int(lat)
-    
-    # Convert sortable integers to binary string
-    lon_bin = bin(lon_sortable_int)[2:].zfill(32)
-    lat_bin = bin(lat_sortable_int)[2:].zfill(32)
-
-    # interleave binary representations
-    z_index_bin = ''.join(
-            start_time_bin[i:i+1] + lon_bin[i:i+1] + lat_bin[i:i+1]
-            for i in range(0, 32, 1)
-    )
-
-    return z_index_bin
 
 
 def generate_events(num_events, output_file):
