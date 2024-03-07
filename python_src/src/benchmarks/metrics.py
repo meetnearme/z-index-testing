f2d89b48ba3dd21639960b9b46c65f881da1f834
Brandon Tripp <brandongtripp@gmail.com>
2024-03-07 15:07:32 -0700

    Correct metrics to parse correct DS types and remove throttled key

--- python_src/src/benchmarks/metrics.py
+++ python_src/src/benchmarks/metrics.py
@@ -1,5 +1,7 @@
 import json
 import time
+import os
+import csv
 
 from datetime import datetime
 
@@ -7,21 +9,26 @@ class MetricsCollector:
     def __init__(self):
         self.metrics = []
 
-    def capture_metrics(self, result, start_time, end_time):
+    def capture_metrics(self, result, metrics, start_time, end_time):
+        print(f"Result in the capture metrics: {metrics}")
         metric = {
-            'read_capacity_units': result['ConsumedCapacity']['CapacityUnits'],
-            'write_capacity_units': result.get('ConsumedCapacity', {}).get('CapacityUnits', 0),
-            'throttled_requests': result['ConsumedCapacity'].get('ThrottledRequests', 0),
-            'conditional_check_failed': result.get('ConditionalCheckFailedCount', 0),
-            'item_size_bytes': sum(len(item.encode('utf-8')) for item in result['Items']),
+            'read_capacity_units': metrics.get('CapacityUnits', 0),
+            'write_capacity_units': metrics.get('CapacityUnits', 0),
+            'conditional_check_failed': metrics.get('ConditionalCheckFailedCount', 0),
+            'item_size_bytes': sum(len(str(item).encode('utf-8')) for item in result),
             'latency': end_time - start_time,
-            'item_count': len(result['Items']),
+            'item_count': len(result),
             'timestamp': time.time()
         }
         self.metrics.append(metric)
 
-    def store_metrics(self, file_path):
+    def store_metrics(self, file_name):
+        directory = 'src/benchmarks/metrics_result'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        file_path = os.path.join(directory, file_name)
         with open(file_path, 'w') as f:
-            for metric in self.metrics:
-                f.write(json.dumps(metric))
-                f.write('\n')
+            writer = csv.DictWriter(f, fieldnames=self.metrics[0].keys())
+            writer.writeheader()
+            writer.writerows(self.metrics)
