6d38604c852cbcd2fc2b81ae35e6e7971cef49a2
Brandon Tripp <brandongtripp@gmail.com>
2024-03-07 15:06:08 -0700

    Update imports for test running script at base level

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from python_src.tests.test_queries import TestSpatialQueries
+from python_src.tests.test_queries import TestSpatialQueriesZOrder
 import unittest
 
 if __name__ == "__main__":
