4cc947e5d613e1f606b47af8e8a7110cdd551607
Brandon Tripp <brandongtripp@gmail.com>
2024-03-07 11:34:16 -0700

    Add calls to caputre and write metrics methods.

--- python_src/src/benchmarks/benchmark_z_order.py
+++ python_src/src/benchmarks/benchmark_z_order.py
@@ -1,22 +1,30 @@
 import pytest
-from .benchmarks import BenchmarkBase
+
+from .benchmarks_base import BenchmarkBase
 from ..queries.z_order import query_point, query_range
 from .query_params import point_query_params, range_query_params
 
 
-class BenchmarkZOrder(BenchmarkBase):
+class TestBenchmarkZOrder(BenchmarkBase):
     def setup_method(self):
         super().setup_method('EventsTableZOrder')
 
     def teardown_method(self):
         super().teardown_method()
 
-    # @pytest.mark.benchmark(group='z_order')
-    def benchmark_query_point(self, benchmark):
-        for params in point_query_params:
-            result = benchmark(query_point, params['lon'], params['lat'])
+    def benchmark(self, benchmark):
+        return benchmark
 
-    # @pytest.mark.benchmark(group='z_order')
-    def benchmark_query_range(self, benchmark):
+    @pytest.mark.benchmark(group='z_order')
+    def test_benchmark_query_point(self, benchmark):
+        for params in point_query_params:
+            result, metrics = benchmark(query_point, params['lon'], params['lat'])
+            self.capture_metrics(result, metrics['start_time'], metrics['end_time'])
+            self.store_metrics('metrics/z_order.csv')
+
+    @pytest.mark.benchmark(group='z_order')
+    def test_benchmark_query_range(self, benchmark):
         for params in range_query_params:
-            result = benchmark(query_range, params['min_lat'], params['max_lat'], params['min_lon'], params['max_lon'])
+            result, metrics = benchmark(query_range, params['min_lat'], params['max_lat'], params['min_lon'], params['max_lon'])
+            self.capture_metrics(result, metrics['start_time'], metrics['end_time'])
+            self.store_metrics('metrics/z_order.csv')
