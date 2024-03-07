b006b36c8d8fe16f69a427f12fbeb20d352a1b59
Brandon Tripp <brandongtripp@gmail.com>
2024-03-07 15:38:52 -0700

    Stub class for visualizer class

---
+++ python_src/src/visualizations/visualizations.py
@@ -0,0 +1,25 @@
+class Visualizer:
+    def __init__(self):
+        self.metrics = {}
+
+    def load_metrics(self, metric_files):
+        pass
+
+    def plot_latency(self):
+        pass
+
+    def plot_read_capacity(self):
+        pass
+
+    def plot_item_count(self):
+        pass
+
+    def plot_all(self):
+        pass
+
+    def plot_scanned_items(self):
+        pass
+
+    def export_plots(self, file_prefix):
+        pass
+
