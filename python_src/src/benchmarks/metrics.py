53871332bdeb9254804d0f11c4fe38a5720d92d1
Brandon Tripp <brandongtripp@gmail.com>
2024-03-08 13:24:56 -0700

    Add function to metrics module to extract response fields for metrics

--- python_src/src/benchmarks/metrics.py
+++ python_src/src/benchmarks/metrics.py
@@ -23,7 +23,7 @@ class MetricsCollector:
         self.metrics.append(metric)
 
     def store_metrics(self, file_name):
-        directory = 'src/benchmarks/metrics_result'
+        directory = 'src/benchmarks/metrics_results'
         if not os.path.exists(directory):
             os.makedirs(directory)
 
@@ -32,3 +32,31 @@ class MetricsCollector:
             writer = csv.DictWriter(f, fieldnames=self.metrics[0].keys())
             writer.writeheader()
             writer.writerows(self.metrics)
+
+
+def extract_metrics(response, start_time):
+    """
+    Extract relevant metrics from the DynamoDB response.
+
+    Args:
+        response (dict): The response received from the DynamoDB query using boto3
+        start_time (float):  The start time of the query in seconds since the epoch
+
+    Returns:
+        dict: A dictionary containing the extracted metrics.
+    """
+    end_time = time.time()
+    metrics = {
+        'start_time': start_time,
+        'end_time': end_time,
+        'read_capacity_units': response.get('ConsumedCapacity', {}).get('CapacityUnits', 0),
+        'write_capacity_units': 0,
+        'conditional_check_failed': response.get('ConditionalCheckFailedCount', 0),
+        'item_size_bytes': sum(len(str(item).encode('utf-8')) for item in response.get('Items', [])),
+        'latency': time.time() - start_time,
+        'item_count': len(response.get('Items', [])),
+        'timestamp': time.time()
+    }
+    return metrics
+
+
