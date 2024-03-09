fcf531fd938b2b66a82d4b98a821f4b8c0bc749d
Brandon Tripp <brandongtripp@gmail.com>
2024-03-08 17:26:13 -0700

    Update generate visualizations script, will rename because generates reports also

--- python_src/scripts/generate_visualizations.py
+++ python_src/scripts/generate_visualizations.py
@@ -1,5 +1,9 @@
 import os
+import sys
 from python_src.src.visualizations.visualizations import Visualizer
+from python_src.src.benchmarks.reporting import generate_report, aggregate_metrics
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 if __name__ == "__main__":
     # Instantiate the visualizer
@@ -7,7 +11,8 @@ if __name__ == "__main__":
 
     # Load the metrics from files
     metric_files = [
-            'metrics/z_order.csv',
+        os.path.join('python_src', 'src', 'benchmarks', 'metrics_results', 'z_order_point.csv'),
+        os.path.join('python_src', 'src', 'benchmarks', 'metrics_results', 'z_order_range.csv'),
     ]
 
     visualizer.load_metrics(metric_files)
@@ -20,4 +25,17 @@ if __name__ == "__main__":
     visualizer.plot_all()
     visualizer.export_plots('benchmark_results')
 
+    # prepare the indexing schemes list for point
+    indexing_schemes = [
+            {'name': 'Z-Order Index Point', 'metrics': aggregate_metrics(visualizer.metrics['z_order_point'])},
+    ]
 
+    # Generate report
+    generate_report(indexing_schemes, 'point')
+
+    # prepare the indexing schemes list for range
+    indexing_schemes = [
+            {'name': 'Z-Order Index Range', 'metrics': aggregate_metrics(visualizer.metrics['z_order_range'])},
+    ]
+
+    generate_report(indexing_schemes, 'range')
