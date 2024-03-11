e6230295226be4bd85bd92875cdc9f044027c7dd
Brandon Tripp <brandongtripp@gmail.com>
2024-03-11 14:07:41 -0600

    Add a min, max and actual flag to deal with handling timestamp suffix for unique idenfication of events. min rounds down to ignore in queries     and max rounds up.

--- python_src/src/indexing/z_order.py
+++ python_src/src/indexing/z_order.py
@@ -21,7 +21,7 @@ def map_float_to_sortable_int(float_value):
     return sortable_int
 
 
-def calculate_z_order_index(start_time, lon, lat):
+def calculate_z_order_index(start_time, lon, lat, index_type='actual'):
     # Get current timestamp as index creation time
     index_creation_time_bin = bin(int(datetime.now().timestamp()))[2:].zfill(32)
 
@@ -31,7 +31,7 @@ def calculate_z_order_index(start_time, lon, lat):
     # Map floating point values to sortable unsigned integers
     lon_sortable_int = map_float_to_sortable_int(lon)
     lat_sortable_int = map_float_to_sortable_int(lat)
-    
+
     # Convert sortable integers to binary string
     lon_bin = bin(lon_sortable_int)[2:].zfill(32)
     lat_bin = bin(lat_sortable_int)[2:].zfill(32)
@@ -43,6 +43,11 @@ def calculate_z_order_index(start_time, lon, lat):
             start_time_bin[i:i+1] + lon_bin[i:i+1] + lat_bin[i:i+1]
             for i in range(0, 32, 1)
     )
+
+    if index_type == 'min':
+        index_creation_time_bin = '0' * 32
+    elif index_type == 'max':
+        index_creation_time_bin = '1' * 32
 
     z_index_with_creation_time_bin = z_index_bin + index_creation_time_bin
 
