0249da6ff6929e082219b305c64f527217393139
Brandon Tripp <brandongtripp@gmail.com>
2024-03-11 17:56:46 -0600

    Refactor map to float sortable int to indexing utils

--- python_src/src/indexing/z_order.py
+++ python_src/src/indexing/z_order.py
@@ -1,27 +1,9 @@
 import struct
 from datetime import datetime
 
-def map_float_to_sortable_int(float_value):
-    """
-    Maps a floating-point value to an unsigned integer that can be lexicographically sorted
-    """
-    float_bytes = struct.pack('>f', float_value)
+from indexing_utils import map_float_to_sortable_int
 
-    # apply mapping function
-    if float_value >= 0:
-        # XOR operation for flipping first bit to make unsigned integer representation of positive float come after negative
-        sortable_bytes = bytes([(float_bytes[0] ^ 0x80)] + list(float_bytes[1:]))
-    else:
-        # XOR to flip all bits, to make negative floats come first from lowest to highest in unsigned integer representation
-        sortable_bytes = bytes([b ^ 0xFF for b in float_bytes])
-
-    # convert mapped bytes to an unsigned integer
-    sortable_int = int.from_bytes(sortable_bytes, byteorder='big', signed=False)
-
-    return sortable_int
-
-
-def calculate_z_order_index(start_time, lon, lat, index_type='actual'):
+def calculate_z_order_index(start_time, lat, lon, index_type='actual'):
     # Get current timestamp as index creation time
     index_creation_time_bin = bin(int(datetime.now().timestamp()))[2:].zfill(32)
 
